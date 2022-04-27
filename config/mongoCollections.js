5fa9192d6f1a721e3c755efec64153b285af1590
Hk6777 <harshilkachhadiya6777@gmail.com>
2022-04-27 17:29:56 -0400

    create database

---
+++ config/mongoCollections.js
@@ -0,0 +1,29 @@
+const dbConnection = require('./mongoConnection');
+
+/* This will allow you to have one reference to each collection per app */
+/* Feel free to copy and paste this this */
+const getCollectionFn = (collection) => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection.connectToDb();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+/* Now, you can list your collections here: */
+module.exports = {
+  user: getCollectionFn('user'),
+  playground: getCollectionFn('playground'),
+  game: getCollectionFn('game'),
+  host: getCollectionFn('host'),
+  players: getCollectionFn('players'),
+  review: getCollectionFn('review'),
+  comment: getCollectionFn('comment'),
+  quiz:getCollectionFn('quiz')
+
+};
