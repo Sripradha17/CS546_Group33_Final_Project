0b2d877db8eb8c04b43fe409a2e55320f0c5d70a
bhargavimangukiya <93874984+bhargavimangukiya@users.noreply.github.com>
2022-05-02 02:52:54 +0530

    Resolve errors

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ const static = express.static(__dirname + '/public');
 const configRoutes = require('./routes');
 const exphbs = require('express-handlebars');
 const session = require('express-session')
+const path = require('path');
 
 app.use('/public', static);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+app.engine('handlebars', exphbs.engine({ 
+  defaultLayout: 'main',
+  layoutsDir: path.join(__dirname, "views", "layouts"),
+  partialsDir: path.join(__dirname, "views", "partials"),
+}));
 app.set('view engine', 'handlebars');
 
 app.use(session({
